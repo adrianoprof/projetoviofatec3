f468a1443fbe5a0d08766c3c37622235782da287
Adriano <adrianosenai604@gmail.com>
2025-11-07 11:08:28 -0300

    feat(login): atualizar título da página

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -96,7 +96,7 @@ function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Vio - Testando um deploy ao vivo
+          Vio - Testando um deploy ao vivo 07/11
         </Typography>
         <Box
           component="form"
